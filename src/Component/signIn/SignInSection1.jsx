bd7d0d0b61eff40fa55391e396e06fda960e7444
habibulla552272 <mdhabibulla146425@gmail.com>
2024-12-13 15:45:56 +0600

    sign In Update

--- src/Component/signIn/SignInSection1.jsx
+++ src/Component/signIn/SignInSection1.jsx
@@ -2,41 +2,67 @@ import React, { useState } from "react";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const SignInSection1 = () => {
-
-    let [email,setEmail]= useState("")
-    let [password, setPassword] = useState("");
-
-    let emailHandel=(e)=>{
-        setEmail(e.target.value);
-    }
-   let passwordHandel = (e) => {
-     setPassword(e.target.value);
-   };
-
-
-
-
-
-
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+  let [emailMessage, setEmailMessage] = useState("");
+  let [passwordMessage, setPasswordMessage] = useState("");
+  let [success, setSuccess] = useState("");
 
   const auth = getAuth();
+
+  let emailHandel = (e) => {
+    setEmail(e.target.value);
+  };
+  let passwordHandel = (e) => {
+    setPassword(e.target.value);
+  };
+
   const signInHandel = () => {
-    createUserWithEmailAndPassword(auth, email, password)
-      .then(() => {
-       console.log('Nice Bro');
-       
-      })
-      .catch((error) => {
-        
-        console.log(error);
-        
-        // ..
-      });
+    if (!email) {
+      setEmailMessage("Email De");
+    } else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+      setEmailMessage("Correct Email  De");
+    } else if (!password) {
+      setEmailMessage("");
+      setPasswordMessage("password de");
+    } else if (!/(?=.*[a-z])/.test(password)) {
+      setEmailMessage("");
+      setPasswordMessage("Please Lowercase");
+    } else if (!/(?=.*[A-Z])/.test(password)) {
+      setEmailMessage("");
+      setPasswordMessage("Please uppderCase");
+    } else if (!/(?=.*[0-9])/.test(password)) {
+      setEmailMessage("");
+      setPasswordMessage("Please number");
+    } else if (!/(?=.{8,})/.test(password)) {
+      setEmailMessage("");
+      setPasswordMessage("Please Minimum 8 digit");
+    } else {
+      setEmailMessage('')
+      setPasswordMessage('')
+      createUserWithEmailAndPassword(auth, email, password)
+        .then(() => {
+          setSuccess("Congratulation MD for Create your account");
+          
+        })
+        .catch((error) => {
+          let err = error.code;
+          if (err.includes("auth/email-already-in-use")) {
+            setSuccess("This Email all Redy existe");
+          } else {
+            setEmailMessage("");
+            setPasswordMessage("");
+          }
+
+          // ..
+        });
+    }
   };
   return (
     <section>
       <div className="container mx-auto py-10">
         <div className="w-11/12 mx-auto md:w-5/12  shadow-2xl px-5 py-5">
+          <p className="py-2 text-red-500 text-xl">{success}</p>
           <h2 className="text-center py-1 text-xl font-josefin font-semibold">
             Sign In
           </h2>
@@ -46,18 +72,24 @@ const SignInSection1 = () => {
 
           <div>
             <div className="flex flex-col gap-8 py-4">
-              <input
-                onChange={(e) => emailHandel(e)}
-                className="w-full outline-none px-2 py-2 border-2 rounded-md"
-                type="email"
-                placeholder="Email Address"
-              />
-              <input
-                onChange={(e) => passwordHandel(e)}
-                className="w-full outline-none px-2 py-2 border-2 rounded-md"
-                type="password"
-                placeholder="Password"
-              />
+              <div>
+                <input
+                  onChange={(e) => emailHandel(e)}
+                  className="w-full outline-none px-2 py-2 border-2 rounded-md"
+                  type="email"
+                  placeholder="Email Address"
+                />
+                <p>{emailMessage}</p>
+              </div>
+              <div>
+                <input
+                  onChange={(e) => passwordHandel(e)}
+                  className="w-full outline-none px-2 py-2 border-2 rounded-md"
+                  type="password"
+                  placeholder="Password"
+                />
+                <p>{passwordMessage}</p>
+              </div>
             </div>
 
             <p className="py-2 cursor-pointer">Forgot your password?</p>
